19a256400c717c09ace9506fdf095731d96e86c7
Mario Naiem Bedrous <62382441+Marioktkt136@users.noreply.github.com>
2024-04-29 16:32:40 +0300

    last 29/4

---
+++ wearther_app/lib/cubits/cubit/weather_cubit_state.dart
@@ -0,0 +1,8 @@
+part of 'weather_cubit_cubit.dart';
+
+@immutable
+class WeatherCubitState {}
+
+class WeatherCubitLoading extends WeatherCubitState {}
+class WeatherCubitSuccess extends WeatherCubitCubit {}
+class WeatherCubitFailure extends WeatherCubitCubit {}
