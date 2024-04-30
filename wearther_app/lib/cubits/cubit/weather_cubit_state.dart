8c95b7a936420f5bc2244efad54daf30b5180fb4
Mario Naiem Bedrous <62382441+Marioktkt136@users.noreply.github.com>
2024-04-30 10:57:42 +0300

    add weather cubit & weather state

--- wearther_app/lib/cubits/cubit/weather_cubit_state.dart
+++
@@ -1,8 +0,0 @@
-part of 'weather_cubit_cubit.dart';
-
-@immutable
-class WeatherCubitState {}
-
-class WeatherCubitLoading extends WeatherCubitState {}
-class WeatherCubitSuccess extends WeatherCubitCubit {}
-class WeatherCubitFailure extends WeatherCubitCubit {}
---
+++ wearther_app/lib/cubits/weather/weather_cubit.dart
@@ -0,0 +1,19 @@
+import 'package:weather_app/cubits/weather/weather_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app/models/weather_model.dart';
+import 'package:weather_app/services/weather_service.dart';
+class WeatherCubit extends Cubit<WeatherState> {
+  WeatherCubit(this.weatherService) : super(WeatherLoading());
+
+  WeatherService weatherService;
+  WeatherModel? weatherModel;
+   void getWeather({required String cityName}) async {
+    emit(WeatherLoading());
+    try {
+      weatherModel = await weatherService.getWeather(cityName: cityName);
+      emit(WeatherSuccess());
+    } catch (e) {
+      emit(WeatherFailure());
+    }
+}
+}
